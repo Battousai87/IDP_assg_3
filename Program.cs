e08e99fe4d27d39233f4985b18c5d1feb42bdbd9
Christian Karlsson <battousai877@gmail.com>
2023-02-24 16:54:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace git_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Element
+    {
+        //namn, Z (atomnummer), typ, smältpunkt, kokpunkt
+        public string Name;
+        public string Z;
+        public string type;
+        public double meltingPoint;
+        public double boilingPoint;
+    }
+
+}
