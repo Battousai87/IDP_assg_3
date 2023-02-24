4fc56a5f2f783bdbf6e8ac51c5330294359f6153
Christian Karlsson <battousai877@gmail.com>
2023-02-24 20:47:58 +0100

    initiated the element objects: oxygen, iron and gold

--- Program.cs
+++ Program.cs
@@ -4,15 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Instansiera tre grundämnen syre, järn och guld och deras data med:
+            syre 8 ickemetall 54.36 90.188
+            järn 26 metall 1811 3134
+            guld 79 metall 1337.33 3243*/
+
+            Element oxygen = new Element();
+            oxygen.name = "oxygen";
+            oxygen.z = "8";
+            oxygen.type = "non-metal";
+            oxygen.meltingPoint = 54.36;
+            oxygen.boilingPoint = 90.188;
+
+            Element iron = new Element();
+            iron.name = "iron";
+            iron.z = "26";
+            iron.type = "metal";
+            iron.meltingPoint = 1811;
+            iron.boilingPoint = 3134;
+
+            Element gold = new Element();
+            gold.name = "gold";
+            gold.z = "79";
+            gold.type = "metal";
+            gold.meltingPoint = 1337.33;
+            gold.boilingPoint = 3243;
+
         }
     }
 
     class Element
     {
         //namn, Z (atomnummer), typ, smältpunkt, kokpunkt
-        public string Name;
-        public string Z;
+        public string name;
+        public string z;
         public string type;
         public double meltingPoint;
         public double boilingPoint;
